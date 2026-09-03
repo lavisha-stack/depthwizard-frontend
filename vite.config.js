ca493553fbde2715cd8dff39010b6bc538375eba
lavisha-stack <lavisha.2642009@gmail.com>
2026-09-03 18:45:46 +0530

    Add Vite configuration for server and build settings

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  server: {
+    port: 5173,
+    host: true,
+  open: false,
+  fs: {
+      allow: ["."],
+    },
+  },
+  build: {
+    outDir: "dist",
+  },
+  publicDir: "data",
+});
